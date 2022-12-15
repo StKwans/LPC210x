8edcb62274ade871fcdc0acf94e97c34220894fe
kwan3217 <kwan3217@gmail.com>
2022-12-14 19:14:12 -0700

    Add driver for Pin Connect Block

---
+++ libraries/System/pinconnect.h
@@ -0,0 +1,27 @@
+//
+// Created by jeppesen on 12/6/22.
+//
+
+#ifndef LPC210X_PINCONNECT_H
+#define LPC210X_PINCONNECT_H
+
+class PinConnectDriver {
+private:
+  static const uint32_t PINSEL_BASE_ADDR=0xE002'C000;
+  static volatile uint32_t& PINSEL0() {return (*(volatile uint32_t*)(PINSEL_BASE_ADDR + 0x00));}
+  static volatile uint32_t& PINSEL1() {return (*(volatile uint32_t*)(PINSEL_BASE_ADDR + 0x04));}
+public:
+  void set_pin(int pin, int mode) {
+    int mask=~(0x3 << ((pin & 0x0F)<<1));
+    int val=mode << ((pin & 0x0F)<<1);
+    if(pin>=16) {
+      PINSEL1()=(PINSEL1() & mask) | val;
+    } else {
+      PINSEL0()=(PINSEL0() & mask) | val;
+    }
+  }
+};
+
+inline PinConnectDriver PinConnect;
+
+#endif //LPC210X_PINCONNECT_H
