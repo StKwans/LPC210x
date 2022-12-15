6944c05f93b32526c22e9e26da1153034f13f959
kwan3217 <kwan3217@gmail.com>
2022-12-14 19:12:24 -0700

    Add code which actually blinks

--- Blink/main.cpp
+++ Blink/main.cpp
@@ -1,9 +1,13 @@
 #include "timer.h"
+#include "gpio.h"
 
 void setup() {
-
+  pinMode(13,true);
 }
 
 void loop() {
+  digitalWrite(13,true);
+  delay(1000);
+  digitalWrite(13,false);
   delay(1000);
 }
