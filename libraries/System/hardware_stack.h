6346e3f4196bd013693ed8114adb977c06b4d97f
kwan3217 <kwan3217@gmail.com>
2022-12-15 08:35:15 -0700

    Re-ordered stack start definitions. Shrank IRQ and FIQ stacks, since 2KiB is not a lot of memory.

--- libraries/System/hardware_stack.h
+++ libraries/System/hardware_stack.h
@@ -14,14 +14,15 @@
 #define ABT_Stack_Size 0  //Likewise
 #endif
 #ifndef FIQ_Stack_Size
-#define FIQ_Stack_Size 128
+#define FIQ_Stack_Size 0
 #endif
 #ifndef IRQ_Stack_Size
-#define IRQ_Stack_Size 512
+#define IRQ_Stack_Size 64
 #endif
 extern int stack_start[];
 extern int ram_end[];
-static inline int USR_Stack_Size() {
+
+static inline __attribute__((always_inline)) int USR_Stack_Size() {
   return (ram_end-stack_start)*sizeof(int)-UND_Stack_Size
                                           -SVC_Stack_Size
                                           -ABT_Stack_Size
@@ -31,12 +32,12 @@ static inline int USR_Stack_Size() {
 
 const int stackPattern=0x6E61774B; // Appears as "Kwan" in little-endian ascii
 
-int * const Stack_UND=ram_end-UND_Stack_Size/sizeof(int);
-int * const Stack_SVC=Stack_UND-SVC_Stack_Size/sizeof(int);
-int * const Stack_ABT=Stack_SVC-ABT_Stack_Size/sizeof(int);
-int * const Stack_FIQ=Stack_ABT-FIQ_Stack_Size/sizeof(int);
-int * const Stack_IRQ=Stack_FIQ-IRQ_Stack_Size/sizeof(int);
 int * const Stack_USR=stack_start;
+int * const Stack_IRQ=Stack_USR+USR_Stack_Size/sizeof(int);
+int * const Stack_FIQ=Stack_IRQ+IRQ_Stack_Size/sizeof(int);
+int * const Stack_ABT=Stack_FIQ+FIQ_Stack_Size/sizeof(int);
+int * const Stack_SVC=Stack_ABT+ABT_Stack_Size/sizeof(int);
+int * const Stack_UND=Stack_SVC+SVC_Stack_Size/sizeof(int);
 
 //Each one returns number of bytes on each stack which have never been touched
 //rounded down to nearest multiple of 4. Negative if every byte has been touched
