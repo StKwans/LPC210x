0dcbe9680a4c5319b3734e2aefd7ecbf98434509
kwan3217 <kwan3217@gmail.com>
2022-12-16 00:46:09 -0700

    Re-ordered stack again. Took away stack from all modes except user.

--- libraries/System/hardware_stack.h
+++ libraries/System/hardware_stack.h
@@ -1,8 +1,6 @@
 #ifndef HARDWARE_STACK_H
 #define HARDWARE_STACK_H
 
-
-
 //Stack sizes are in bytes
 #ifndef UND_Stack_Size
 #define UND_Stack_Size 0  //No stack for you! You are just an infinite loop
@@ -17,7 +15,7 @@
 #define FIQ_Stack_Size 0
 #endif
 #ifndef IRQ_Stack_Size
-#define IRQ_Stack_Size 64
+#define IRQ_Stack_Size 0
 #endif
 extern int stack_start[];
 extern int ram_end[];
@@ -32,12 +30,12 @@ static inline __attribute__((always_inline)) int USR_Stack_Size() {
 
 const int stackPattern=0x6E61774B; // Appears as "Kwan" in little-endian ascii
 
-int * const Stack_USR=stack_start;
-int * const Stack_IRQ=Stack_USR+USR_Stack_Size/sizeof(int);
+int * const Stack_IRQ=stack_start;
 int * const Stack_FIQ=Stack_IRQ+IRQ_Stack_Size/sizeof(int);
 int * const Stack_ABT=Stack_FIQ+FIQ_Stack_Size/sizeof(int);
 int * const Stack_SVC=Stack_ABT+ABT_Stack_Size/sizeof(int);
 int * const Stack_UND=Stack_SVC+SVC_Stack_Size/sizeof(int);
+int * const Stack_USR=Stack_UND+UND_Stack_Size/sizeof(int);
 
 //Each one returns number of bytes on each stack which have never been touched
 //rounded down to nearest multiple of 4. Negative if every byte has been touched
