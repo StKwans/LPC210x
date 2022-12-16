cdb3afd0e2a769d5c8a60849e207260061175b86
kwan3217 <kwan3217@gmail.com>
2022-12-16 08:06:23 -0700

    Made Serial a header-only module. Removed interrupt stuff for now.

--- libraries/Serial/Serial.h
+++ libraries/Serial/Serial.h
@@ -24,89 +24,80 @@
 
 #include <inttypes.h>
 #include "Stream.h"
-#include "LPC214x.h"
 
-//#define SERIAL_IRQ
-
-#ifdef SERIAL_IRQ
-#include "Circular.h"
-#endif
-
+template<int port>
 class HardwareSerial: public Stream {
-  private:
-#ifdef SERIAL_IRQ
-    char rxbuf[128];
-    Circular rx;
-    friend void uart0_isr();
-    friend void uart1_isr();
-#endif
-  public:
-    unsigned int port;
-    HardwareSerial(int Lport):
-#ifdef SERIAL_IRQ
-      rx(sizeof(rxbuf),rxbuf),
-#endif
-      port(Lport) {};
-    HardwareSerial(int Lport, unsigned int baud):HardwareSerial(Lport) {begin(baud);};
-    HardwareSerial(int Lport, unsigned int baud, const char* msg):HardwareSerial(Lport,baud) {println(msg);};
-    void begin(unsigned int baud);
-    void end();
-    void listen();
-    int available(void) override;
-    int peek(void) override;
-    int read(void) override;
-    void flush(void) override;
-    void write(uint8_t) override;
-    using Print::write; // pull in write(str) and write(buf, size) from Print
+private:
+public:
+  HardwareSerial() {};
+  void begin(unsigned int baud) {
+    //Set up the pins
+    if(port==0) {
+      set_pin(0,1); //TX0
+      set_pin(1,1); //RX0
+    } else {
+      set_pin(8,1); //TX1
+      set_pin(9,1); //RX1
+    }
+    ULCR(port) = 0x83;   // 8 bits, no parity, 1 stop bit, DLAB = 1
+    //DLAB - Divisor Latch Access bit. When set, a certain memory address
+    //       maps to the divisor latches, which control the baud rate. When
+    //       cleared, those same addresses correspond to the processor end
+    //       of the FIFOs. In other words, set the DLAB to change the baud
+    //       rate, and clear it to use the FIFOs.
+
+    unsigned int Denom=Time::PCLK/baud;
+    unsigned int UDL=Denom/16;
+
+    UDLM(port)=(UDL >> 8) & 0xFF;
+    UDLL(port)=(UDL >> 0) & 0xFF;
+
+    UFCR(port) = 0xC7; //Enable and clear both FIFOs, Rx trigger level=14 chars
+    ULCR(port) = 0x03; //Turn of DLAB - FIFOs accessable
+    UIER(port)=0;
+  };
+  void end() void HardwareSerial::end() {
+    //set the pins to read (high Z)
+    if(port==0) {
+      set_pin(0,0); //TX0->GPIO
+      set_pin(1,0); //RX0->GPIO
+      gpio_set_read(0);
+      gpio_set_read(1);
+    } else {
+      set_pin(8,0); //TX1->GPIO
+      set_pin(9,0); //RX1->GPIO
+      gpio_set_read(8);
+      gpio_set_read(9);
+    }
+  };
+  void listen() {};
+  int available(void) override {
+    int result=((ULSR(port) & 0x01)>0)?1:0;
+    return result;
+  };
+  int peek(void) override {
+    //Peek not supported for a pure hardware read
+    return -1;
+  };
+  int read(void) override {
+    if(available()>0) {
+      return URBR(port);
+    } else {
+      return -1;
+    }
+  }
+  void flush(void) override {
+    UFCR(port)|=0x07;
+  }
+  void write(uint8_t c) override {
+    while (!(ULSR(port) & 0x20));
+    UTHR(port) = c;
+  };
+  using Print::write; // pull in write(str) and write(buf, size) from Print
 };
 
-inline int HardwareSerial::available(void) {
-#ifdef SERIAL_IRQ
-  int result=rx.readylen();
-#else
-  int result=((ULSR(port) & 0x01)>0)?1:0;
-#endif
-//  print("A: ");
-//  println(result);
-  return result;
-}
-
-inline int HardwareSerial::read(void) {
-  if(available()>0) {
-#ifdef SERIAL_IRQ
-    return rx.get();
-#else
-    return URBR(port);
-#endif
-  } else {
-    return -1;
-  }
-}
-
-inline void HardwareSerial::flush(void) {
-  UFCR(port)|=0x07;
-#ifdef SERIAL_IRQ
-  rx.empty();
-#endif
-}
-
-inline void HardwareSerial::write(uint8_t c) {
-  while (!(ULSR(port) & 0x20));
-  UTHR(port) = c;
-}
-
-inline int HardwareSerial::peek(void) {
-#ifdef SERIAL_IRQ
-  return rx.peekTail();
-#else
-  //Peek not supported for a pure hardware read
-  return -1;
-#endif
-}
-
 //These are predeclared so that everyone can use them, but must be defined manually.
-extern HardwareSerial Serial;
-extern HardwareSerial Serial1;
-extern int testInit;
+inline HardwareSerial<0> Serial;
+inline HardwareSerial<1> Serial1;
 
 #endif
