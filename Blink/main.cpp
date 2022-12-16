56c85d806ff20d2755251af04f38683837e45ba1
kwan3217 <kwan3217@gmail.com>
2022-12-16 08:03:38 -0700

    Blink works, but only with direct_blink(), not with normal pinMode() and digitalWrite().

--- Blink/main.cpp
+++ Blink/main.cpp
@@ -1,13 +1,13 @@
-#include "timer.h"
 #include "gpio.h"
 
 void setup() {
+  GPIODriver::direct_blink();
   pinMode(13,true);
 }
 
 void loop() {
   digitalWrite(13,true);
-  delay(1000);
+  for(int i=0;i<1'000'000;i++) {}
   digitalWrite(13,false);
-  delay(1000);
+  for(int i=0;i<1'000'000;i++) {}
 }
