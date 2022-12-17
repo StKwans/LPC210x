fc11d95ac16477d9da72af2c1cd01f89e81c9b53
kwan3217 <kwan3217@gmail.com>
2022-12-16 17:10:48 -0700

    Added register definitions for serial port

--- libraries/Serial/Serial.h
+++ libraries/Serial/Serial.h
@@ -24,55 +24,72 @@
 
 #include <inttypes.h>
 #include "Stream.h"
+#include "pinconnect.h"
+#include "scb.h"
 
 template<int port>
 class HardwareSerial: public Stream {
 private:
+/* Universal Asynchronous Receiver Transmitter */
+  static const uint32_t UART0_BASE_ADDR	=0xE000'C000;
+  static const uint32_t UART1_BASE_ADDR	=0xE001'0000;
+  static const uint32_t UART_BASE_DELTA =(UART1_BASE_ADDR-UART0_BASE_ADDR);
+  static volatile uint32_t  URBR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x00));}
+  static volatile uint32_t& UTHR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x00));}
+  static volatile uint32_t& UDLL() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x00));}
+  static volatile uint32_t& UDLM() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x04));}
+  static volatile uint32_t& UIER() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x04));}
+  static volatile uint32_t  UIIR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x08));}
+  static volatile uint32_t& UFCR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x08));}
+  static volatile uint32_t& ULCR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x0C));}
+  static volatile uint32_t& UMCR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x10));}
+  static volatile uint32_t  ULSR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x14));}
+  static volatile uint32_t& UMSR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x18));}
+  static volatile uint32_t& USCR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x1C));}
+  static volatile uint32_t& UACR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x20));}
+  static volatile uint32_t& UFDR() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x28));}
+  static volatile uint32_t& UTER() {return (*(volatile uint32_t*)(UART0_BASE_ADDR+(port)*UART_BASE_DELTA + 0x30));}
 public:
   HardwareSerial() {};
   void begin(unsigned int baud) {
     //Set up the pins
     if(port==0) {
-      set_pin(0,1); //TX0
-      set_pin(1,1); //RX0
+      PinConnect.set_pin(0,1); //TX0
+      PinConnect.set_pin(1,1); //RX0
     } else {
-      set_pin(8,1); //TX1
-      set_pin(9,1); //RX1
+      PinConnect.set_pin(8,1); //TX1
+      PinConnect.set_pin(9,1); //RX1
     }
-    ULCR(port) = 0x83;   // 8 bits, no parity, 1 stop bit, DLAB = 1
+    ULCR() = 0x83;   // 8 bits, no parity, 1 stop bit, DLAB = 1
     //DLAB - Divisor Latch Access bit. When set, a certain memory address
     //       maps to the divisor latches, which control the baud rate. When
     //       cleared, those same addresses correspond to the processor end
     //       of the FIFOs. In other words, set the DLAB to change the baud
     //       rate, and clear it to use the FIFOs.
 
-    unsigned int Denom=Time::PCLK/baud;
+    unsigned int Denom=SCB.PCLK()/baud;
     unsigned int UDL=Denom/16;
 
-    UDLM(port)=(UDL >> 8) & 0xFF;
-    UDLL(port)=(UDL >> 0) & 0xFF;
+    UDLM()=(UDL >> 8) & 0xFF;
+    UDLL()=(UDL >> 0) & 0xFF;
 
-    UFCR(port) = 0xC7; //Enable and clear both FIFOs, Rx trigger level=14 chars
-    ULCR(port) = 0x03; //Turn of DLAB - FIFOs accessable
-    UIER(port)=0;
+    UFCR() = 0xC7; //Enable and clear both FIFOs, Rx trigger level=14 chars
+    ULCR() = 0x03; //Turn of DLAB - FIFOs accessable
+    UIER()=0;
   };
-  void end() void HardwareSerial::end() {
+  void end() {
     //set the pins to read (high Z)
     if(port==0) {
-      set_pin(0,0); //TX0->GPIO
-      set_pin(1,0); //RX0->GPIO
-      gpio_set_read(0);
-      gpio_set_read(1);
+      PinConnect.set_pin(0,0); //TX0->GPIO
+      PinConnect.set_pin(1,0); //RX0->GPIO
     } else {
-      set_pin(8,0); //TX1->GPIO
-      set_pin(9,0); //RX1->GPIO
-      gpio_set_read(8);
-      gpio_set_read(9);
+      PinConnect.set_pin(8,0); //TX1->GPIO
+      PinConnect.set_pin(9,0); //RX1->GPIO
     }
   };
   void listen() {};
   int available(void) override {
-    int result=((ULSR(port) & 0x01)>0)?1:0;
+    int result=((ULSR() & 0x01)>0)?1:0;
     return result;
   };
   int peek(void) override {
@@ -81,17 +98,17 @@ public:
   };
   int read(void) override {
     if(available()>0) {
-      return URBR(port);
+      return URBR();
     } else {
       return -1;
     }
   }
   void flush(void) override {
-    UFCR(port)|=0x07;
+    UFCR()|=0x07;
   }
   void write(uint8_t c) override {
-    while (!(ULSR(port) & 0x20));
-    UTHR(port) = c;
+    while (!(ULSR() & 0x20));
+    UTHR() = c;
   };
   using Print::write; // pull in write(str) and write(buf, size) from Print
 };
